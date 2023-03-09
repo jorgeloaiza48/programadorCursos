ecc0e13537a8aebbe09f3585fad14ecf6abc989e
jorgeloaiza48 <jelm48@misena.edu.co>
2023-03-09 07:56:43 -0500

    organizando presentación

--- programador-backend/server/controller.js
+++ programador-backend/server/controller.js
@@ -2,8 +2,8 @@
 //const cors = require('cors');
 // const app = express()
 // const bodyParser = require('body-parser');
-const {email} = require('./credentials')
-const {password} = require('./credentials')
+const { email } = require('./credentials')
+const { password } = require('./credentials')
 const fs = require('fs')
 const path = require("path");
 const jwt = require('jsonwebtoken')
@@ -20,7 +20,7 @@ const controller = {
         let NewUser = []
         NewUser = JSON.parse(users) //JSON a JS
         let id = NewUser.length + 1
-    
+
         let nuevoUsuario = {
             id: id,
             email: req.body.email,
@@ -44,8 +44,79 @@ const controller = {
                 fs.writeFileSync(usersFilePath, JSON.stringify(NewUser, null, "\t")) //de JS a JSON           
                 res.status(200).send("Usuario creado")
                 //res.send("Usuario creado")
-    
+
             }
+        }
+    },
+
+    updateUser: (req, res) => {
+        let usersFilePath = path.join(__dirname, './usuariosRegistrados.json');
+        User = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8')) //JSON a JS      
+        User.find(element => {
+            if (element.email === req.body.email) {
+                element.coordenadasCurso.push(req.body.coordenadasCurso)
+                element.colorDeRelleno.push(req.body.colorRelleno)
+                element.coordColorHoras.push(req.body.coordColorHoras)
+                element.totalHorasPorMes = [] //se borra el array para que no se acumulen los datos de forma innecesaria
+                element.totalHorasPorMes.push(req.body.totalHorasPorMes)
+            }
+        });
+        fs.writeFileSync(usersFilePath, JSON.stringify(User, null, "\t")) //de JS a JSON
+        res.send("Usuario actualizado con éxito")
+    },
+
+    borrarTodaLaProgramacion: (req, res) => {
+        let usersFilePath = path.join(__dirname, './usuariosRegistrados.json');
+        User = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8')) //JSON a JS      
+        User.find(element => {
+            if (element.email === req.body.email) {
+                element.coordenadasCurso = []
+                element.colorDeRelleno = []
+                element.coordColorHoras = []
+                element.totalHorasPorMes = []
+            }
+        });
+        fs.writeFileSync(usersFilePath, JSON.stringify(User, null, "\t")) //de JS a JSON
+        res.send("Programación borrada con éxito")
+    },
+
+    borrarUnCurso: (req, res) => {
+        let usersFilePath = path.join(__dirname, './usuariosRegistrados.json');
+        User = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8')) //JSON a JS   
+        User.find(element => {
+            if (element.email === req.body.email) {
+                for (let i = 0; i < element.coordColorHoras.length; i++) {
+                    let resultado = false
+                    resultado = element.coordColorHoras[i].includes(req.body.color.replace(/ /g, "")) //replace(/ /g, "") quita los epacios en blanco intermedios. Por ejemplo rgb(12, 56, 125) y queda rgb(12,56,125). El color llegaba desde el front con espacios en blancos intermedios y por eso no había una coincidencia en la búsquedad. https://es.stackoverflow.com/questions/165669/como-eliminar-los-espacios-en-blanco-en-un-string                
+                    if (resultado) {
+                        let p = 0
+                        //Este ciclo actualiza el arreglo donde están las horas totales de cada mes
+                        for (let j = 2; j < element.coordColorHoras[i].length; j = j + 3) {
+                            element.totalHorasPorMes[0][p] = element.totalHorasPorMes[0][p] - element.coordColorHoras[i][j]
+                            p++
+                        }
+                        element.coordColorHoras.splice(i, 1) //borra las coordenadas del curso que se quiere eliminar
+                        element.colorDeRelleno.splice(i, 1) //borra el color del curso que se quiere borrar
+                        element.coordenadasCurso.splice(i, 1) // borra la coordenada, el color y la hora parcial del curso que se desea borrar.
+                    }
+                }
+            }
+        })
+        fs.writeFileSync(usersFilePath, JSON.stringify(User, null, "\t")) //de JS a JSON         
+        res.send("El curso seleccionado se borró con éxito")
+    },
+
+    login: (req, res) => {
+        let usersFilePath = path.join(__dirname, './usuariosRegistrados.json');
+        let users = fs.readFileSync(usersFilePath, 'utf-8')
+        let user = []
+        user = JSON.parse(users) //JSON a JS   
+        let userFilter = user.filter(element => (element.email === req.body.email && element.password === req.body.password))
+        if (userFilter.length !== 0) {
+            res.status(200).send('Usuario encontrado')
+        }
+        else {
+            res.status(400).send('Usuario NO encontrado')
         }
     },
 
@@ -129,6 +200,12 @@ const controller = {
 
 
 
+    },
+    usuariosRegistrados: (req, res) => {
+        let usersFilePath = path.join(__dirname, './usuariosRegistrados.json');
+        let users = fs.readFileSync(usersFilePath, 'utf-8')
+        NewUser = JSON.parse(users) //JSON a JS
+        res.json(NewUser) //formato jsob
     }
 }
 module.exports = controller
--- programador-backend/server/index.js
+++ programador-backend/server/index.js
@@ -35,88 +35,16 @@ app.use(
 //     res.send()
 //     //next(); 
 // })
+
 app.post('/create-user', controller.createUser)
-
-app.post('/update-user', function (req, res) {
-    let usersFilePath = path.join(__dirname, './usuariosRegistrados.json');
-    User = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8')) //JSON a JS      
-    User.find(element => {
-        if (element.email === req.body.email) {
-            element.coordenadasCurso.push(req.body.coordenadasCurso)
-            element.colorDeRelleno.push(req.body.colorRelleno)
-            element.coordColorHoras.push(req.body.coordColorHoras)
-            element.totalHorasPorMes = [] //se borra el array para que no se acumulen los datos de forma innecesaria
-            element.totalHorasPorMes.push(req.body.totalHorasPorMes)
-        }
-    });
-    fs.writeFileSync(usersFilePath, JSON.stringify(User, null, "\t")) //de JS a JSON
-    res.send("Usuario actualizado con éxito")
-})
-
-app.post('/borrar-toda-programacion', function (req, res) {
-    let usersFilePath = path.join(__dirname, './usuariosRegistrados.json');
-    User = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8')) //JSON a JS      
-    User.find(element => {
-        if (element.email === req.body.email) {
-            element.coordenadasCurso = []
-            element.colorDeRelleno = []
-            element.coordColorHoras = []
-            element.totalHorasPorMes = []
-        }
-    });
-    fs.writeFileSync(usersFilePath, JSON.stringify(User, null, "\t")) //de JS a JSON
-    res.send("Programación borrada con éxito")
-})
-
-app.post('/borrar-curso', function (req, res) {
-    let usersFilePath = path.join(__dirname, './usuariosRegistrados.json');
-    User = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8')) //JSON a JS   
-    User.find(element => {
-        if (element.email === req.body.email) {
-            for (let i = 0; i < element.coordColorHoras.length; i++) {
-                let resultado = false
-                resultado = element.coordColorHoras[i].includes(req.body.color.replace(/ /g, "")) //replace(/ /g, "") quita los epacios en blanco intermedios. Por ejemplo rgb(12, 56, 125) y queda rgb(12,56,125). El color llegaba desde el front con espacios en blancos intermedios y por eso no había una coincidencia en la búsquedad. https://es.stackoverflow.com/questions/165669/como-eliminar-los-espacios-en-blanco-en-un-string                
-                if (resultado) {
-                    let p = 0
-                    //Este ciclo actualiza el arreglo donde están las horas totales de cada mes
-                    for (let j = 2; j < element.coordColorHoras[i].length; j = j + 3) {
-                        element.totalHorasPorMes[0][p] = element.totalHorasPorMes[0][p] - element.coordColorHoras[i][j]
-                        p++
-                    }
-                    element.coordColorHoras.splice(i, 1) //borra las coordenadas del curso que se quiere eliminar
-                    element.colorDeRelleno.splice(i, 1) //borra el color del curso que se quiere borrar
-                    element.coordenadasCurso.splice(i, 1) // borra la coordenada, el color y la hora parcial del curso que se desea borrar.
-                }
-            }
-        }
-    })
-    fs.writeFileSync(usersFilePath, JSON.stringify(User, null, "\t")) //de JS a JSON         
-    res.send("El curso seleccionado se borró con éxito")
-})
-
-app.post("/login",(req,res) =>{
-    let usersFilePath = path.join(__dirname, './usuariosRegistrados.json');
-    let users = fs.readFileSync(usersFilePath, 'utf-8')
-    let user = []
-    user = JSON.parse(users) //JSON a JS   
-    let userFilter = user.filter(element => (element.email === req.body.email && element.password === req.body.password) )    
-    if(userFilter.length !== 0){
-        res.status(200).send('Usuario encontrado')
-    }
-    else{
-        res.status(400).send('Usuario NO encontrado')
-    }
-})
+app.post('/update-user', controller.updateUser )
+app.post('/borrar-toda-programacion', controller.borrarTodaLaProgramacion )
+app.post('/borrar-curso', controller.borrarUnCurso)
+app.post("/login",controller.login)
 app.post("/forgot-password",controller.forgotPassword)
 app.get('/reset-password/:id/:token', controller.resetPassword)
 app.post('/reset-password/:id/:token', controller.CambioPassword)
-
-app.get("/usuarios-registrados", (req, res) => {
-    let usersFilePath = path.join(__dirname, './usuariosRegistrados.json');
-    let users = fs.readFileSync(usersFilePath, 'utf-8')
-    NewUser = JSON.parse(users) //JSON a JS
-    res.json(NewUser) //formato jsob
-})
+app.get("/usuarios-registrados", controller.usuariosRegistrados)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
