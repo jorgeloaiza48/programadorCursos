d5306a3e6980843db5e71f5f29b2bba7c9e863f8
jorgeloaiza48 <jelm48@misena.edu.co>
2023-03-09 19:39:27 -0500

    arreglando

--- programador-frontend/src/index.js
+++ programador-frontend/src/index.js
@@ -4,15 +4,10 @@ import ReactDOM from 'react-dom/client';
 import Router from './components/Router';
 import './index.css';
 //import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
- 
-    
-    <Router/>
-  
-);
+root.render( <Router/>);
 
 
-reportWebVitals();
+//reportWebVitals();
